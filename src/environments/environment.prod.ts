b107db83828f50c6c40c4c5e2b747ea046fcdac0
JohnnyQuintero16 <johnnyaquintero@gmail.com>
2022-09-22 19:45:48 -0500

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  URL_PRODUCTO_API : 'http://localhost:8098/api/v1/productos/',
+  URL_AUTH_API : 'http://localhost:8098/api/v1/auth/',
 };
