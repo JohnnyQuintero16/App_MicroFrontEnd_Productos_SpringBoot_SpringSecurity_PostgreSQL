bfa95fc743ecc2adfa01fef0907b6ffc21452546
JohnnyQuintero16 <johnnyaquintero@gmail.com>
2022-09-22 20:27:47 -0500

    deploy produ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  URL_PRODUCTO_API : 'http://localhost:8098/api/v1/productos/',
-  URL_AUTH_API : 'http://localhost:8098/api/v1/auth/',
+  URL_PRODUCTO_API : 'https://arquiproductos.herokuapp.com/api/v1/auth/api/v1/productos/',
+  URL_AUTH_API : 'https://arquiproductos.herokuapp.com/api/v1/auth/api/v1/auth/',
 };
